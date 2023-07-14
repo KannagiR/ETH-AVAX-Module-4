2771999c1d14bce42cb3c54131750d4673fe5282
KannagiR <136475740+KannagiR@users.noreply.github.com>
2023-07-14 22:06:43 +0530

    module4.sol

--- module4.sol
+++ module4.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.0;
 contract DegenToken {
     string public name;
     string public symbol;
-    uint8 public decimals;
+    uint256 public decimals;
     uint256 public totalSupply;
 
     mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowances;
-
     address public owner;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
