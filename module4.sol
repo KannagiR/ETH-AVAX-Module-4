176384b10d64e3b54ac929cc8c675dc3a8d06062
KannagiR <136475740+KannagiR@users.noreply.github.com>
2023-07-17 23:13:05 +0530

    module4.sol

--- module4.sol
+++ module4.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 contract DegenToken {
     string public name;
     string public symbol;
-    uint256 public decimals;
     uint256 public totalSupply;
 
     mapping(address => uint256) private balances;
+    
     address public owner;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -15,24 +15,23 @@ contract DegenToken {
     event Burn(address indexed from, uint256 value);
     event Redeem(address indexed from, string itemName);
     
+    
     string[] public items; 
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function.");
-        _;
-    }
-
     constructor() {
         name = "Degen Gaming Token";
         symbol = "DGN";
-        decimals = 15;
-        totalSupply = 0;
+        totalSupply = 100000;
         owner = msg.sender;
-
-        items.push("Hoodie");
+        
+        
         items.push("T-shirt");
         items.push("Bagpack");
         items.push("Surprise");
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
     }
     // tranfer tokens
     function transfer(address recipient, uint256 amount) external returns (bool) {
@@ -47,13 +46,6 @@ contract DegenToken {
         balances[to] += amount;
         emit Mint(to, amount);
     }
-    // burn tokens
-    function burn(uint256 amount) external {
-        require(amount <= balances[msg.sender], "No available tokens");
-        balances[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Burn(msg.sender, amount);
-    }
     // redeem tokens
     function redeem() external returns (string memory) {
         require(balances[msg.sender] > 500, "Insufficient tokens to redeem");
@@ -62,14 +54,25 @@ contract DegenToken {
         string memory chosenItem = items[randomIndex];
 
         
-        uint256 redemptionAmount = 500; 
+        uint256 redemptionAmount = 300; 
+        require(balances[msg.sender] >= redemptionAmount, "Insufficient tokens to redeem selected item");
         balances[msg.sender] -= redemptionAmount;
         emit Redeem(msg.sender, items[randomIndex]);
         return chosenItem;
+    }
+    // burn tokens
+    function burn(uint256 amount) external {
+        require(amount <= balances[msg.sender], "No available tokens");
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Burn(msg.sender, amount);
     }
     // fetch balance
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
     }
+    // check owner
+    function getOwner() external view returns (address) {
+        return owner;
+    }
 }
-
